888bae7f5ae09773897d7a99d28883caf6f075cc
Lucas Silva <lucaszsilva.dev@gmail.com>
2026-08-16 19:55:49 -0300

    feat(configs): add initial project configuration

---
+++ src/main/java/com/github/tluccas/notifyhub/NotifyhubApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.tluccas.notifyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyhubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyhubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tluccas/notifyhub/NotifyhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tluccas.notifyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyhubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
