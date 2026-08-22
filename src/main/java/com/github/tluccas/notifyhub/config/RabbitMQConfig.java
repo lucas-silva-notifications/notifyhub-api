05dad459a0d7af6c97ec43796b8596d8e632306e
Lucas Silva <lucaszsilva.dev@gmail.com>
2026-08-22 16:31:02 -0300

    feat(rabbitmq): add rabbitmq setup

---
+++ src/main/java/com/github/tluccas/notifyhub/config/RabbitMQConfig.java
@@ -0,0 +1,36 @@
+package com.github.tluccas.notifyhub.config;
+
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String NOTIFICATION_EXCHANGE = "notification.events";
+    public static final String EMAIL_NOTIFICATION_QUEUE = "email-notification.queue";
+    public static final String EMAIL_NOTIFICATION_ROUTING_KEY = "notification.requested.email";
+
+    @Bean()
+    public TopicExchange notificationExchange() {
+        return new TopicExchange(NOTIFICATION_EXCHANGE, true, false);
+    }
+
+    @Bean()
+    public Queue emailNotificationQueue() {
+        return QueueBuilder.durable(EMAIL_NOTIFICATION_QUEUE)
+                .build();
+    }
+
+    @Bean()
+    public Binding emailNotificationBinding() {
+        return BindingBuilder.bind(emailNotificationQueue())
+        .to(notificationExchange())
+        .with(EMAIL_NOTIFICATION_ROUTING_KEY);
+    }
+    
+}
---
+++ src/main/java/com/github/tluccas/notifyhub/config/RabbitMessageConverterConfig.java
@@ -0,0 +1,24 @@
+package com.github.tluccas.notifyhub.config;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMessageConverterConfig {
+    
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter converter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(converter);
+        return template;
+    }
+}
